aa7f975a0839d0ec45ed9443aa7398c19099b8f4
Carlos Cimino <ciminoce@yahoo.com.ar>
2022-06-16 19:23:03 -0300

    Agregar archivos de proyecto.

---
+++ NroComplejoPOO2022.Consola/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NroComplejoPOO2022.Entidades;
+
+namespace NroComplejoPOO2022.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Complex numbers!!");
+            var nc = new NumeroComplejo(10, -5);
+            var nc2=new NumeroComplejo(10,-5);
+            var nc3 = nc + nc2;
+            var nc4=new NumeroComplejo(1,2);
+            var nc5 = nc - nc4;
+
+            var nc6 = "10-2i";
+            Console.WriteLine(nc6.ToString());
+            Console.WriteLine(nc.ToString());
+            Console.WriteLine(nc2.ToString());
+            Console.WriteLine(nc3.ToString());
+            Console.WriteLine(nc4.ToString());
+            Console.WriteLine(nc5.ToString());
+            if (nc==nc2)
+            {
+                Console.WriteLine("Ambos números son iguales");
+            }
+            else
+            {
+                Console.WriteLine("Son distintos");
+            }
+        }
+    }
+}
---
+++ NroComplejoPOO2022.Entidades/NumeroComplejo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace NroComplejoPOO2022.Entidades
+{
+    public class NumeroComplejo
+    {
+        private int ParteEntera;
+        private int ParteImaginaria;
+
+        public NumeroComplejo(int parteEntera, int parteImaginaria)
+        {
+            ParteEntera = parteEntera;
+            ParteImaginaria = parteImaginaria;
+        }
+
+        public NumeroComplejo(int parteEntera) : this(parteEntera, 0)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            if (ParteEntera == 0)
+            {
+                return $"{ParteImaginaria}i";
+            }
+            if (ParteImaginaria < 0)
+            {
+                return $"{ParteEntera}{ParteImaginaria}i";
+            }
+            return $"{ParteEntera}+{ParteImaginaria}i";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj==null || !(obj is NumeroComplejo))
+            {
+                return false;
+            }
+
+            return this.ParteEntera == ((NumeroComplejo) obj).ParteEntera &&
+                   this.ParteImaginaria == ((NumeroComplejo) obj).ParteImaginaria;
+        }
+
+        public static bool operator ==(NumeroComplejo c1, NumeroComplejo c2)
+        {
+            return c1.Equals(c2);
+        }
+
+        public static bool operator !=(NumeroComplejo c1, NumeroComplejo c2)
+        {
+            return !(c1 == c2);
+        }
+
+        public static NumeroComplejo operator +(NumeroComplejo c1, NumeroComplejo c2)
+        {
+            return new NumeroComplejo(c1.ParteEntera+c2.ParteEntera, c1.ParteImaginaria+c2.ParteImaginaria);
+        }
+
+        public static NumeroComplejo operator -(NumeroComplejo c1, NumeroComplejo c2)
+        {
+            var pEntera = c1.ParteEntera - c2.ParteEntera;
+            var pImaginaria = c1.ParteImaginaria - c2.ParteImaginaria;
+            return new NumeroComplejo(pEntera,pImaginaria);
+        }
+
+        public static NumeroComplejo operator *(NumeroComplejo c1, NumeroComplejo c2)
+        {
+            var pEntera = c1.ParteEntera * c2.ParteEntera - (c1.ParteImaginaria * c2.ParteImaginaria);
+            var pImaginaria = c1.ParteEntera * c2.ParteImaginaria + (c1.ParteImaginaria * c2.ParteEntera);
+            return new NumeroComplejo(pEntera, pImaginaria);
+        }
+
+        public static NumeroComplejo operator +(NumeroComplejo c1, int n)
+        {
+            return new NumeroComplejo(c1.ParteEntera+n,c1.ParteImaginaria);
+        }
+
+        public static implicit operator NumeroComplejo(string s)
+        {
+            var signo = s.Contains('+') ? '+' : '-';
+            var array = s.Split(signo);
+
+            var parteEntera = int.Parse(array[0]);
+            var parteImaginaria =int.Parse(array[1].ToString().Replace('i', ' ').Trim());
+            return signo=='-' ? new NumeroComplejo(parteEntera,-parteImaginaria) : new NumeroComplejo(parteEntera,parteImaginaria);
+        }
+
+    }
+}
